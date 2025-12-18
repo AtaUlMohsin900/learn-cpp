92282c0916c52c04ad5fc479772e6c4d880f7f5f
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-12-18 18:42:23 +0000

    update

--- projects/calcultor.cpp
+++ projects/calcultor.cpp
@@ -18,3 +18,31 @@ int main() {
     cout << "\t\t****************************************   WAEL   *************************************" << endl;
     cout << "-------------------------------------------------------------------------------------------------------"
          << endl;
+         
+    cout << "Basic calculator\t'a'\nAdvanced calculator\t'b'\nASCII Table\t\t'c'\nDigital system\t\t'd'\n" << ">> ";
+    cin >> option;
+    if (option == 'a') {
+        double x, y, z = 0;
+        cout << "Enter first number , operation , second number (x+y)\n>> ";
+        cin >> x >> op >> y;
+        if (op == '+')
+            z += x + y;
+        else if (op == '-')
+            z += x - y;
+        else if (op == '*')
+            z += x * y;
+        else if (op == '/')
+            z += x / y;
+        cout << "Result = " << z << endl;
+    } else if (option == 'b') {
+        double x, y, z = 1;
+        cout << "Square root (z^2=x)\t'a'\nnth Root\t(z^y=x)\t'b'\npower square\t(x^2)\t'c'\nPower nth\t(x^y)\t'd'\n";
+        cout << "*Note ('x' first number , 'y' second number , 'z' result, '^' power sign)\n>> ";
+        cin >> op;
+        if (op == 'a')// sqrt
+        {
+            cout << "Enter the number of (x) : ";
+            cin >> x;
+            if (x < 0) {
+                cout << "Error!\nPlease, Check the input!";
+            }
