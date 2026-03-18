fcb5d1fd5343ecf24f9859a5f4c32988d6f12f96
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-03-18 18:29:57 +0000

    update

---
+++ oop/userdefine_datatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Student{// Student is a new datatype
+	public:
+		string name;
+		int rollnum;
+		int age;
+		float cgpa;
+};
+
+int main(){
+    Student s;
+    s.name = "ataulmohsin";
+    s.rollnum = 458;
+    s.age = 45;
+    s.cgpa = 3.56;
+	return 0;	
+}
