0a92f2740391b692c9fd37c8a48ffc79107ebb78
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-05-10 02:48:19 +0000

    do_while_loop

---
+++ loops.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+// While Loop
+// int main() {
+//   int i = 0;
+//   while (i < 5) {
+//     cout << i << "\n";
+//     i++;
+//   }
+//   return 0;
+// }
+
+// Do While Loop
+
+int main() {
+  int i = 0;
+  do {
+    cout << i << "\n";
+    i++;
+  }
+  while (i < 5);
+  return 0;
+}
