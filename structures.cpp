0a2ffb06e02041d4c7be569c26fc8c38487d866e
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-06-12 02:40:54 +0000

    structures

--- structures.cpp
+++ structures.cpp
@@ -1,4 +1,9 @@
-// Create a structure variable called myStructure
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // Create a structure variable called myStructure
 struct {
   int myNum;
   string myString;
@@ -11,3 +16,8 @@ myStructure.myString = "Hello World!";
 // Print members of myStructure
 cout << myStructure.myNum << "\n";
 cout << myStructure.myString << "\n";
+  
+  return 0;
+}
+
+
