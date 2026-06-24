b2f96af94f57c88ffef6c84f3dd646dd6e27ea6e
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-06-24 11:16:56 +0000

    private class

--- oop/accessSpecifiers.cpp
+++ oop/accessSpecifiers.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 using namespace std;
 
-class MyClass {   // The class
-  public:         // Public access specifier
-    string x;        // Public attribute (int variable)
+// class MyClass {   // The class
+//   public:         // Public access specifier
+//     string x;        // Public attribute (int variable)
+// };
+
+class MyClass {
+  public:    // Public access specifier
+    int x;   // Public attribute
+  private:   // Private access specifier
+    int y;   // Private attribute
 };
 
 int main() {
-  MyClass myObj;  // Create an object of MyClass
+//   MyClass myObj;  // Create an object of MyClass
 
-  // Access attributes and set values
-  myObj.x = "hello world!";
+//   // Access attributes and set values
+//   myObj.x = "hello world!";
 
-  // Print values
-  cout << myObj.x; 
+//   // Print values
+//   cout << myObj.x; 
+
+
+int main() {
+  MyClass myObj;
+  myObj.x = 25;  // Allowed (x is public)
+  myObj.y = 50;  // Not allowed (y is private)
+  return 0;
+}
+
+
   return 0;
 }
