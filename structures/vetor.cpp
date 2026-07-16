0c48a930fdc41300cac65c6784e0f21b13de8462
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-07-16 01:40:42 +0000

    vetor in c++

---
+++ structures/vetor.cpp
@@ -0,0 +1,16 @@
+// Include the vector library
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  // Create a vector called cars that will store strings
+  vector<string> cars = {"Toyota", "Dongfing", "GMC", "Nissan"};
+
+  // Get the first element
+  cout << cars[0] << "\n";
+  
+  // Get the second element
+  cout << cars[1] << "\n";
+  return 0;
+}
