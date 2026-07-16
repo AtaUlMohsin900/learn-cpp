835c162b35e06d85d2cfdade9fbf072980283626
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-07-16 02:16:22 +0000

    update

--- structures/vetor.cpp
+++ structures/vetor.cpp
@@ -1,4 +1,5 @@
-// Include the vector library
+// vector is a dynamic array from the STL that automatically resize when you add or remove elements.
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -14,3 +15,29 @@ int main() {
   cout << cars[1] << "\n";
   return 0;
 }
+
+// Function            What it does
+// v.push_back(x)      Add element at end
+// v.pop_back()        Remove last element
+// v.size()            Number of elements
+// v.clear()           Removes all elements
+// v.front()           First element
+// v.back()            Last element
+// v.empty()           Returns true if empty
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+m̧pty
