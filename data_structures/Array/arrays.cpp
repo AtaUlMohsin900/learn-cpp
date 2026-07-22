7220b7d3d840bd4153c76b797e49eac44c45397c
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-07-22 01:06:32 +0000

    arrays

---
+++ data_structures/Array/arrays.cpp
@@ -0,0 +1,22 @@
+// Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+// Arrays solve our multiple veriabls store problem.
+
+// we should same data type store in arrays.
+// Arrays are contiguous in memory and also are liner.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int numbers[5] = {100, 55 , 30, 45, 57}
+    // auto save in arrays memory
+    double int marks[] = {75.9 , 58.2 , 45.4 , 64.4, 34.4};
+
+    cout << mark[0] <<endl;
+    cout << mark[1] <<endl;
+    cout << mark[2] <<endl;
+    cout << mark[3] <<endl;
+    cout << mark[4] <<endl;
+
+    return 0;
+}
