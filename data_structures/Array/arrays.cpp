6f3dcc8f197aa06b9cf662228c9d7216d066dde8
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-07-22 01:22:58 +0000

    update

--- data_structures/Array/arrays.cpp
+++ data_structures/Array/arrays.cpp
@@ -8,15 +8,24 @@
 using namespace std;
 
 int main(){
-    // int numbers[5] = {100, 55 , 30, 45, 57}
+    int numbers[5] = {100, 55 , 30, 45, 57}
     // auto save in arrays memory
-    double int marks[] = {75.9 , 58.2 , 45.4 , 64.4, 34.4};
+    // double int marks[] = {75.9 , 58.2 , 45.4 , 64.4, 34.4};
+    // change value of arrays also posible.
+    // marks[2] = 65.5;
+    // cout << mark[0] <<endl;
+    // cout << mark[1] <<endl;
+    // cout << mark[2] <<endl;
+    // cout << mark[3] <<endl;
+    // cout << mark[4] <<endl;
+// we should access the value of array in limit of array otherwise it returns garbage value.
 
-    cout << mark[0] <<endl;
-    cout << mark[1] <<endl;
-    cout << mark[2] <<endl;
-    cout << mark[3] <<endl;
-    cout << mark[4] <<endl;
+// We gerneraly use loops when we perform operations on arrays.
+cout << sizeof(marks) / sizeof(int) <<endl;
+for(int i=0; i<size; i++){
+    cout << marks[i] << endl;
+}
+
 
     return 0;
 }
