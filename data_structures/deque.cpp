b118132a26136bd3a3fb9ed52023fc7649f1e764
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-07-24 01:55:40 +0000

    update

--- data_structures/deque.cpp
+++ data_structures/deque.cpp
@@ -10,12 +10,24 @@
 using namespace std;
 
 int main() {
-  // Create a deque called cars that will store strings
-  deque<string> books = {"English", "Math", "Phisics", "Computer Science"};
+  // Create a deque called books that will store strings
+  // deque<string> books = {"English", "Math", "Phisics", "Computer Science"};
 
-  // Print deque elements
-  for (string book : books) {
-    cout << book << "\n";
-  }
+  // // Print deque elements
+  // for (string book : books) {
+  //   cout << book << "\n";
+  // }
+  
+
+
+  // Create a deque called books that will store strings
+  deque<string> books = "English", "Math", "Phisics", "Computer Science"};
+
+  // Get the first element
+  cout << books.front() << "\n";  // Outputs English
+
+  // Get the last element
+  cout << books.back() << "\n";  // Outputs Math
+
   return 0;
 }
