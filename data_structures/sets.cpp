b3432741011e8afd82e1f04f2ef1b63d4c3b1cde
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-07-25 02:13:41 +0000

    sets in C++

---
+++ data_structures/sets.cpp
@@ -0,0 +1,26 @@
+// A set stores unique elements where they:
+
+// Are sorted automatically in ascending order.
+// Are unique, meaning equal or duplicate values are ignored.
+// Can be added or removed, but the value of an existing element cannot be changed.
+// Cannot be accessed by index numbers, because the order is based on sorting and not indexing.
+
+// Include the set library
+// #include <set>
+
+
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+  // Create a set called numbers that will store integers
+  set<int> numbers = {1, 7, 3, 2, 5, 9};
+
+  // Print set elements
+  for (int num : numbers) {
+    cout << num << "\n";
+  }
+  return 0;
+}
