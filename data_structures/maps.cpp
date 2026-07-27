d1d1357146eb748ddf5070f48e374f4ba2cc450f
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-07-27 01:00:16 +0000

    Maps in C++

---
+++ data_structures/maps.cpp
@@ -0,0 +1,30 @@
+// A map stores elements in "key/value" pairs. map is like a phonebook:
+
+
+// Elements in a map are:
+// Accessible by keys (not index), and each key is unique.
+// Key/value pairs = Name → Phone number
+// Keys are unique – no duplicates
+// Automatically sorted in ascending order by their keys.
+// Auto-sorted by keys (A to Z, 1 to 10)
+// Find by key (not by position like a list)
+// To use a map, you have to include the <map> header file:
+
+// Include the map library #include <map>
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+  // Create a map that will store the name and age of different people
+  map<string, int> employee = { {"Waseem", 54}, {"Amjed", 21}, {"Amir", 46} };
+
+  // Get the value associated with the key "Waseem"
+  cout << "Waseem is: " << employee["Waseem"] << "\n"; 
+
+  // Get the value associated with the key "Amjed"
+  cout << "Amjed is: " << employee["Amjed"] << "\n";
+
+  return 0;
+}
