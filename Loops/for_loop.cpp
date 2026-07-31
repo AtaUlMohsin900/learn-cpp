fb874fceffc0376647fad7fd8f98e4d5abeb3b3c
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-07-31 02:27:50 +0000

    update

---
+++ Loops/for_loop.cpp
@@ -0,0 +1,16 @@
+// When you know exactly how many times you want to loop through a block of code,
+//  use the for loop instead of a while loop.
+
+// syntax: for (statement 1; statement 2; statement 3)
+  // {} code block to be executed
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i = 0; i <= 10, i++ ){
+        cout << i << "\n";
+
+    }
+    return 0;
+}
