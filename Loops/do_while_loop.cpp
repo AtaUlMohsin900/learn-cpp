0bee26e3483a4b9427bf9a2f74fdd6cf5df40821
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-07-31 02:10:14 +0000

    loop_update

---
+++ Loops/do_while_loop.cpp
@@ -0,0 +1,26 @@
+// Do While Loop
+
+int main() {
+//   int i = 0;
+//   do {
+//     cout << i << "\n";
+//     i++;
+//   }
+//   while (i < 5);
+
+
+//   int i = 10;
+//   do {
+//     cout << "i is " << i << "\n";
+//     i++;
+//   } while (i < 5);
+
+
+  int number;
+  do {
+    cout << "Enter a positive number: ";
+    cin >> number;
+  } while (number > 0);
+
+  return 0;
+}
---
+++ Loops/while_loop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+// While Loop
+// int main() {
+//   int i = 0;
+//   while (i < 5) {
+//     cout << i << "\n";
+//     i++;
+//   }
+//   return 0;
+// }
+
+
+int main() {
+  int countdown = 3;
+
+  while (countdown > 0) {
+    cout << countdown << "\n";
+    countdown--;
+  }
+
+  cout << "Learn CPP\n";
+
+  return 0;
+}
