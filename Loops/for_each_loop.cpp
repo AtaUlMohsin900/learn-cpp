d9ce9e4e139c1893ce2ef5f6249e3bdac03ca79c
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-07-31 02:49:17 +0000

    for_each

---
+++ Loops/for_each_loop.cpp
@@ -0,0 +1,32 @@
+// A for-each loop is a special type of loop that automatically goes through every single 
+// item in a collection (like an array) without needing to worry about:
+
+// 1. Starting point
+// 2. Ending point
+// 3. How many items there are
+// 4. Incrementing counters
+
+// The for-each loop is like having a personal assistant who:
+
+// Takes your collection
+// Hands you each item one by one
+// Tells you when you're done
+// Never makes a counting mistake!
+
+// It makes your code cleaner, safer, and easier to read — especially when working with arrays, vectors, and other collections!
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Create an array of integers
+//   int myNumbers[5] = {10, 20, 30, 40, 50};
+  
+  // Loop through integers
+//   
+string word = "hello coders";
+  for (char c : word) {
+    cout << c << "\n";
+  }
+  return 0;
+}
