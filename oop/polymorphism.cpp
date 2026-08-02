494fc7497d7716071195f34cae8f549a334458ae
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-08-02 01:16:29 +0000

    update_polymorphism

--- oop/polymorphism.cpp
+++ oop/polymorphism.cpp
@@ -1,3 +1,16 @@
+
+// Polymorphism means "many forms", and it occurs when we have many classes that are related to each other by inheritance.
+
+// lets us inherit attributes and methods from another class. 
+// Polymorphism uses those methods to perform different tasks. This allows us to perform a single action in different ways.
+
+// For example, imagine a base class Vehicals with a method called makeSound(). Derived classes of Vehicals could be 
+// , Riksha, Train etc. Every vehical can "make a sound", but each one sounds different:
+
+// Riksha sound: tuk tuk
+// Train sound: chick chick
+// This is polymorphism - the same action (making a sound) behaves differently for each vehicals:
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -19,7 +32,7 @@ class Riksha : public vehicals {
 };
 
 // Derived class
-class TTrain : public vehicals {
+class Train : public vehicals {
   public:
     void vehicalsSound() {
       cout << "The train sound is : chik chik \n";
