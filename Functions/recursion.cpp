ed431fb017b196365d3abcf27a45eecb689f7c89
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-08-03 02:59:42 +0000

    recursion in cpp

---
+++ Functions/recursion.cpp
@@ -0,0 +1,23 @@
+// where the function call itself we call it Recursion technique .
+// Adding two numbers together is easy to do, but adding a range of numbers is more complicated.
+
+// In the following example, recursion is used to add a range of numbers together by breaking 
+// it down into the simple task of adding two numbers:
+
+
+#include <iostream>
+using namespace std;
+
+int sum(int x) {
+  if (x > 0) {
+    return x + sum(x - 1);
+  } else {
+    return 0;
+  }
+}
+
+int main() {
+  int result = sum(10);
+  cout << result;
+  return 0;
+}
