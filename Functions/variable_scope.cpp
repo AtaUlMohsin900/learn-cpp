255b444f20c87a61980c712dc535c19fee61e7fc
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-08-03 02:50:04 +0000

    Scope of verables for funcitons

--- Functions/variable_scope.cpp
+++ Functions/variable_scope.cpp
@@ -9,14 +9,35 @@
 #include <iostream>
 using namespace std;
 
-void myfunc(){
-    // local veriable that belongs to myfunc.
-    int x = 5;
+// void myfunc(){
+//     // local veriable that belongs to myfunc.
+//     int x = 5;
 
-    cout << x;
+//     cout << x;
+// }
+
+// int main(){
+//     // myfunc();
+//     // myfun "local function" connot be use out of scope
+//     cout << x;
+//     return 0;
+// }
+
+// if we created veriable outside of a function, it called a global variable and belongs to the global scope.
+// when we create veriable as global scope then we access them in outside of function or inside of function.
+
+int x = 10; // Global scope
+
+void myfunc(){
+    // Local variable with the same name as the global variable (x)
+    int x = 15; //avoid using the same variable name for both globally and locally variables as it can lead to errors and confusion.
+    cout << ++x << "\n"; // inside the scope & increment the value of x by 1 and print it
+
 }
 
 int main(){
-    myfunc();
+    myfun();
+    cout << x; // using global scope
     return 0;
 }
+
