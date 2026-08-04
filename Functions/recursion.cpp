5a21be368cabf87013afffb750037d28e5077260
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-08-04 18:15:08 +0000

    update

--- Functions/recursion.cpp
+++ Functions/recursion.cpp
@@ -8,16 +8,22 @@
 #include <iostream>
 using namespace std;
 
-int sum(int x) {
-  if (x > 0) {
-    return x + sum(x - 1);
-  } else {
-    return 0;
+// int sum(int x) {
+//   if (x > 0) {
+//     return x + sum(x - 1);
+//   } else {
+//     return 0;
+//   }
+// }
+ int main() {
+//   int result = sum(10);
+//   cout << result;
+  countdown(5);
+  return 0;
+}
+    countdown(n - 1); 
   }
 }
 
-int main() {
-  int result = sum(10);
-  cout << result;
   return 0;
 }
