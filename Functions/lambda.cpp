597e39523330578ee6c2f62b495448bf7b6660d1
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-08-05 02:34:44 +0000

    without auto

--- Functions/lambda.cpp
+++ Functions/lambda.cpp
@@ -8,10 +8,19 @@
 using namespace std;
 
 int main(){
-    auto message = [](){
+    auto message = [](){ //auto tells the compiler: "Hey compiler, figure out the type for me". The compiler knows the exact (but unnamed) type of the lambda and assigns it to the variable.
         cout << "Hello World! \n";
 
         message();
         retrun 0;
-    }
+    }; // The semicolon goes after the closing brace } of the lambda body.
+
+// Alternative Ways (Without auto)
+// This works but has overhead (type erasure)
+    std::function<void()> velcale = [](){ 
+        cout << "Toyota" << endl; 
+    };
+    velcale();
+    
 }
+
