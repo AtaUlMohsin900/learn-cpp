60b699eb0865f2e514ff69332e4a6418ee19c091
ataulmohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2026-08-21 06:47:18 +0000

    update_algorithms

--- data_structures/algorithm.cpp
+++ data_structures/algorithm.cpp
@@ -54,5 +54,17 @@ int main() {
   for (int num : numbers) {
     cout << num << "\n";
   }
+
+  // Create a vector called numbers that will store integers
+  vector<int> numbers = {1, 7, 3, 5, 9, 2};
+
+  // Sort numbers numerically, starting from the fourth element (only sort 5, 9, and 2)
+  sort(numbers.begin() + 3, numbers.end());
+
+  // Print numbers
+  for (int num : numbers) {
+    cout << num << "\n";
+  }
+
   return 0;
 }
